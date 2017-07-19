25626c327911af26a3b4b45e9eeafadb4c1fd39e
IBTS\ninganagouda.patil <ninganagouda.patil@incedoinc.com>
2017-07-19 17:10:25 +0530

    adding........

--- src/test/java/com/example/demo/ApplicationTests.java
+++ src/test/java/com/example/demo/ApplicationTests.java
@@ -5,6 +5,8 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+//Testing For Ninganagouda Branch
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
