31bc929b8f7c4de5cef6090434b1264f047607ab
IBTS\ninganagouda.patil <ninganagouda.patil@incedoinc.com>
2017-07-19 15:42:12 +0530

    sdfsdfsdf

--- src/main/java/com/boot/config/Application.java
+++ src/main/java/com/boot/config/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.SpringServletContainerInitializer;
 
+// Boot application starts from here.
 @SpringBootApplication
 @ComponentScan( basePackages = {"com.boot"})
 public class Application extends SpringServletContainerInitializer {
